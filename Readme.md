4cb88424d7c6a16157844731e1a5451337d14e7a
Volodymyr Klymniuk <Volodymyr.Klymniuk@gmail.com>
2023-02-13 02:12:07 +0200

    commit transmition

---
+++ Readme.md
@@ -0,0 +1,2 @@
+# lambda-bridge-js
+A bridge between Lambda functions and Event Handlers
--- test/lambda_bridge_test.js
+++ test/lambda_bridge_test.js
@@ -1,3 +1,4 @@
 'use strict';
 require('./bootstrap.js');
+
 const LambdaBridge = require('../lib/lambda_bridge');
